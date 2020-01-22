7940e935b63a1f3c15de355605281ab4638f3d8b
Gregoire Gobert <gregoire.gobert@ecl17.ec-lyon.fr>
2020-01-22 13:02:14 +0100

    Nouveau menu

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ var burger_menu_open=document.getElementById("burger_menu_open");
 var burger_menu_close=document.getElementById("burger_menu_close");
 
 
-document.getElementById("left_menu").style.left="-4vw";
+document.getElementById("left_menu").style.left="-100vw";
 
 function openMenu()
 {
 	document.getElementById("left_menu").style.left="0vw";
 	document.getElementById("burger_menu_open").style.display="none";
 	document.getElementById("burger_menu_close").style.display="block";
-	document.getElementById("burger_menu_full").style.left="5vw";
+	document.getElementById("burger_menu_full").style.left="1vw";
 }
 
 function closeMenu()
 {
-	document.getElementById("left_menu").style.left="-4vw";
+	document.getElementById("left_menu").style.left="-100vw";
 	document.getElementById("burger_menu_open").style.display="block";
 	document.getElementById("burger_menu_close").style.display="none";
 	document.getElementById("burger_menu_full").style.left="1vw";
